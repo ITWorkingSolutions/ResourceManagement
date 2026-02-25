2580a41944ade9b889dfac0c6e6a96380fd2be0d
IT Working Solutions <117948901+ITWorkingSolutions@users.noreply.github.com>
2026-02-25 20:45:08 +1000

    Delete App Config directory

--- App Config/ListItemSystemReferenceMap.vb
+++
@@ -1,8 +0,0 @@
-﻿Friend Module ListItemSystemReferenceMap
-
-  Friend ReadOnly ListItemSystemReferences As New List(Of ListItemSystemReference) From {
-    New ListItemSystemReference(ListItemTypeSystemCatalog.SalutationTypeID, "tblResource", "SalutationID"),
-    New ListItemSystemReference(ListItemTypeSystemCatalog.GenderTypeID, "tblResource", "GenderID")
-  }
-
-End Module
